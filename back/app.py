b012dc167d25f6a9ece358014ee23c1219ce2d32
liuwllm <liu.wllm@gmail.com>
2024-06-21 17:50:38 -0400

    Initialize Word DB model

--- back/app.py
+++ back/app.py
@@ -18,3 +18,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Database Models
+
+class Word(db.Model):
+    __tablename__ = 'words'
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    keb = db.Column(db.ARRAY(db.String))
+    reb = db.Column(db.ARRAY(db.String))
+    sense = db.Column(db.ARRAY(db.String))
+
+    def __init__(self, id, keb=None, reb=None, sense=None):
+        self.id = id
+        self.keb = keb
+        self.reb = reb
+        self.sense = sense
